52b8f6b30c91bde8d80e5a19e627a4b184db6080
vineet.manohar <vineet.manohar@3cde3dc6-6a6e-11de-8e18-cf3ac8cf5931>
2009-09-01 22:17:53 +0000

    simple jaxb wrapper for validating, xml to java and java to xml for sitemap files

---
+++ src/main/java/com/vineetmanohar/sitemap/SitemapJAXBWrapper.java
@@ -0,0 +1,94 @@
+package com.vineetmanohar.sitemap;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xml.sax.SAXException;
+
+import com.vineetmanohar.sitemap.jaxb.Urlset;
+
+/**
+ * Simple JAXB Wrapper for Sitemap
+ * 
+ * @author Vineet Manohar
+ */
+public class SitemapJAXBWrapper {
+    private static Log log = LogFactory.getLog(SitemapJAXBWrapper.class);
+    private static JAXBContext jaxbContext = null;
+    private static SchemaFactory schemaFactory = null;
+
+    private static Schema schema;
+
+    private static boolean initialized = false;
+
+    static {
+        try {
+            init();
+        } catch (JAXBException e) {
+            throw new RuntimeException("Could not intialize class", e);
+        } catch (SAXException e) {
+            throw new RuntimeException("Could not intialize class", e);
+        }
+    }
+
+    private static Marshaller createMarshaller(Schema schema) throws JAXBException, PropertyException {
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setSchema(schema);
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(true));
+        return marshaller;
+    }
+
+    private static Unmarshaller createUnmarshaller(Schema schema) throws JAXBException {
+        Unmarshaller nsfUnmarshaller = jaxbContext.createUnmarshaller();
+        nsfUnmarshaller.setSchema(schema);
+        return nsfUnmarshaller;
+    }
+
+    private static Marshaller createMarshaller() throws JAXBException {
+        return createMarshaller(schema);
+    }
+
+    private static Unmarshaller createUnmarshaller() throws JAXBException {
+        return createUnmarshaller(schema);
+    }
+
+    public static String toXml(Urlset urlset) throws JAXBException {
+        StringWriter writer = new StringWriter();
+        createMarshaller().marshal(urlset, writer);
+        return writer.toString();
+    }
+
+    private static void init() throws JAXBException, SAXException {
+        if (initialized) {
+            log.info("jaxb context already initialized");
+            return;
+        }
+
+        jaxbContext = JAXBContext.newInstance(Urlset.class);
+        schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+
+        schema = schemaFactory.newSchema(SitemapJAXBWrapper.class.getResource("/sitemap.xsd"));
+
+        log.info("jaxb context initialized");
+        initialized = true;
+    }
+
+    public static void validate(Urlset urlSet) throws JAXBException {
+        createMarshaller().marshal(urlSet, new StringWriter());
+    }
+
+    public static Urlset xmlToObject(String xml) throws JAXBException {
+        StringReader stringReader = new StringReader(xml);
+        return (Urlset) createUnmarshaller().unmarshal(stringReader);
+    }
+}
